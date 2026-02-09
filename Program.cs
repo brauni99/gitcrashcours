ca8167984361f56e894557666b426b60e6953e13
Lukas <lukas.braunsteiner@gmail.com>
2026-02-09 14:03:49 +0100

    commit staged

--- Program.cs
+++ Program.cs
@@ -4,3 +4,13 @@ Console.WriteLine("Ich bin Lukas");
 Console.WriteLine(  "3");
 Console.WriteLine("master");
 Console.WriteLine(  "after git checkout");
+
+
+Console.WriteLine("start countdown, Press any key");
+
+Console.ReadKey();
+for (int i = 0; i < 10; i++)
+{
+    Thread.Sleep(1000);
+    Console.WriteLine(i);
+}
