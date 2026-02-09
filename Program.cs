e37405e0171a57b0bf673ccbfdafbf371c845901
Lukas <lukas.braunsteiner@gmail.com>
2026-02-09 10:03:07 +0100

    changes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Ich bin Lukas");
+Console.WriteLine(  "3");
+Console.WriteLine("master");
